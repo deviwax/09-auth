3d3582124b6ccb25762a340a594b3cbf8a101e49
deviwax <156660735+deviwax@users.noreply.github.com>
2026-06-08 14:44:09 +0300

    update

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+//const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 
 export const nextServer = axios.create({
-  baseURL: BASE_URL,
+  baseURL: 'https://notehub-api.goit.study',
   withCredentials: true,
 });
